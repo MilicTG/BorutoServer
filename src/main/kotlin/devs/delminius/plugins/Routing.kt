f4040b16effe7ae6e6962d20ed2253a32fd6b38f
MilicTG <milic.tg@gmail.com>
2021-12-15 12:47:23 +0100

    Finished video 21

--- src/main/kotlin/devs/delminius/plugins/Routing.kt
+++ src/main/kotlin/devs/delminius/plugins/Routing.kt
@@ -2,13 +2,20 @@ package devs.delminius.plugins
 
 import devs.delminius.routes.getAllHeroes
 import devs.delminius.routes.root
+import devs.delminius.routes.searchHeroes
 import io.ktor.routing.*
 import io.ktor.application.*
+import io.ktor.http.content.*
 
 
 fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+        searchHeroes()
+
+        static(remotePath = "/images") {
+            resources(resourcePackage = "images")
+        }
     }
 }
--- src/main/kotlin/devs/delminius/routes/AllHeroes.kt
+++ src/main/kotlin/devs/delminius/routes/AllHeroes.kt
@@ -1,20 +1,27 @@
 package devs.delminius.routes
 
 import devs.delminius.models.ApiResponse
+import devs.delminius.repository.HeroRepository
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import org.koin.ktor.ext.inject
 import java.lang.IllegalArgumentException
 
 fun Route.getAllHeroes() {
+    val heroRepository: HeroRepository by inject()
+
     get(path = "/boruto/heroes") {
         try {
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
             require(page in 1..5)
 
+            val apiResponse = heroRepository.getAllHeroes(page = page)
+
             call.respond(
-                message = page
+                message = apiResponse,
+                status = HttpStatusCode.OK
             )
         } catch (e: NumberFormatException) {
             call.respond(
