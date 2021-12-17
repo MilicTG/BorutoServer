08e194a835589270ef85cc53eda0149dbb082b62
MilicTG <milic.tg@gmail.com>
2021-12-17 08:54:33 +0100

    finished video 24

--- src/test/kotlin/devs/delminius/ApplicationTest.kt
+++ src/test/kotlin/devs/delminius/ApplicationTest.kt
@@ -1,13 +1,21 @@
 package devs.delminius
 
+import devs.delminius.models.ApiResponse
+import devs.delminius.repository.HeroRepository
 import io.ktor.http.*
 import io.ktor.application.*
 import kotlin.test.*
 import io.ktor.server.testing.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.koin.java.KoinJavaComponent.inject
 
 class ApplicationTest {
+
+    private val heroRepository: HeroRepository by inject(HeroRepository::class.java)
+
     @Test
-    fun testRoot() {
+    fun `access root endpoint, assert correct information`() {
         withTestApplication(moduleFunction = Application::module) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(
@@ -17,6 +25,33 @@ class ApplicationTest {
                 assertEquals(
                     expected = "Welocome to Boeuto API!",
                     response.content
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `access all heroes endpoint, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+                val expected = ApiResponse(
+                    success = true,
+                    message = "ok",
+                    prevPage = null,
+                    nextPage = 2,
+                    heroes = heroRepository.page1,
+                    lastUpdated = System.currentTimeMillis()
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString())
+                println("EXPECTED $expected")
+                println("ACTUAL $actual")
+                assertEquals(
+                    expected = expected,
+                    actual = actual
                 )
             }
         }
